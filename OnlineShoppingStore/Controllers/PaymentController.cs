81b1ed2c25fe04f314e481e70e7e3d5c64a123dc
Shruti Rupani <srupani8595@conestogac.on.ca>
2020-05-06 11:23:06 -0400

    Add project files.

---
+++ OnlineShoppingStore/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class PaymentController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+    }
+}
